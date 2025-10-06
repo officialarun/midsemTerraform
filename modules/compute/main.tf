3ee0778089998b7f46cab4a4773761d1dd3525f0
Ubuntu <arun@dock.lkn2xppttnuujkmj2glmu3uefg.hx.internal.cloudapp.net>
2025-10-06 18:11:38 +0000

    this is first commit

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,51 +1,39 @@
-# Public IP
-resource "azurerm_public_ip" "pip" {
-  name                = "${var.vm_name}-pip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# Network Security Group
 resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.vm_name}-nsg"
+  name                = "nginx-vm-nsg"
   location            = var.location
   resource_group_name = var.resource_group_name
 }
 
-# NSG rules
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "Allow-SSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
 resource "azurerm_network_security_rule" "http" {
   name                        = "Allow-HTTP"
-  priority                    = 100
+  priority                    = 1002
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
-  source_port_range            = "*"
-  destination_port_range       = "80"
-  source_address_prefix        = "*"
-  destination_address_prefix   = "*"
-  resource_group_name          = var.resource_group_name
-  network_security_group_name  = azurerm_network_security_group.nsg.name
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
-resource "azurerm_network_security_rule" "https" {
-  name                        = "Allow-HTTPS"
-  priority                    = 101
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range            = "*"
-  destination_port_range       = "443"
-  source_address_prefix        = "*"
-  destination_address_prefix   = "*"
-  resource_group_name          = var.resource_group_name
-  network_security_group_name  = azurerm_network_security_group.nsg.name
-}
-
-# Network Interface
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
+  name                = "nginx-vm-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
 
@@ -53,36 +41,34 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id           = azurerm_public_ip.pip.id
   }
 }
 
-# Associate NSG to NIC
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+  network_interface_id          = azurerm_network_interface.nic.id
+  network_security_group_id     = azurerm_network_security_group.nsg.id
 }
 
-# Linux VM
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = var.vm_name
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = var.vm_size
-  admin_username        = var.admin_user
-  admin_password        = var.admin_password
+  name                            = "nginx-vm"
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  size                            = "Standard_B1s"
+  admin_username                  = "arun"
+  admin_password                  = "Pandey@2024Azure!"
   disable_password_authentication = false
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+
   os_disk {
+    name                 = "nginx-vm-osdisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
-  publisher = "Canonical"
-  offer     = "UbuntuServer"
-  sku       = "20_04-lts"  # Gen1 version, works in most regions
-  version   = "latest"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
 }
-}
-
